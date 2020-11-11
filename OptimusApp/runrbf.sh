78d0aacab763fdc1efbbba5f7e172f61a741dc33
Ioannis G. Tsoulos <itsoulos@gmail.com>
2020-11-11 17:29:27 +0200

    add doc

---
+++ OptimusApp/runrbf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+DATAPATH=/home/sheridan/Desktop/ERGASIES/FeatureConstruction2/datasets/tenfolding/
+#DATAPATH=/home/sheridan/Desktop/ERGASIES/SECDATA/
+NODES=10
+
+METHOD=Multistart
+METHODPARAMS=""
+if [ $METHOD = "Minfinder" ]
+then
+	METHODPARAMS="--minfinder_samples=25 --minfinder_sampling=repulsion"
+elif [ $METHOD = "Genetic" ]
+then
+	METHODPARAMS="--localsearch_rate=0.00 --localsearch_method=bfgs"
+elif [ $METHOD = "Pso" ]
+then
+	METHODPARAMS="--pso_localsearch_rate=0.01"
+elif [ $METHOD = "Multistart" ]
+then
+	METHODPARAMS=="--multistart_samples=25"
+fi
+
+PROBLEM=RbfTest
+NODES=$2
+
+MLPARAMS="--trainName=$DATAPATH/$1.train --testName=$DATAPATH/$1.test --nodes=$NODES"
+echo ./OptimusApp --filename=../PROBLEMS/RbfTest/lib$PROBLEM.so  --opt_method=$METHOD   $METHODPARAMS  $MLPARAMS --interval_generations=50 --threads=12  --iterations=30
+./OptimusApp --filename=../PROBLEMS/RbfTest/lib$PROBLEM.so  --opt_method=$METHOD   $METHODPARAMS  $MLPARAMS --interval_generations=50 --threads=12  --iterations=30
+
