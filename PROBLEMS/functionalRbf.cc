6bdd90826ea75bf2598155f3af2cbf8d148657cc
Ioannis G. Tsoulos <itsoulos@gmail.com>
2021-01-04 14:04:24 +0200

    Improve RBF with new KMEANS

--- PROBLEMS/functionalRbf.cc
+++ PROBLEMS/functionalRbf.cc
@@ -1,4 +1,5 @@
 # include <math.h>
+# include <stdlib.h>
 # include <vector>
 # include <stdio.h>
 # include <iostream>
@@ -77,8 +78,7 @@ private:
     int clusterId;
     vector<double> centroid;
     vector<Point> points;
-
-
+    vector<double> variance;
 public:
     vector<double> getCentroid() { return centroid; }
     Cluster(int clusterId, Point centroid){
@@ -87,6 +87,28 @@ public:
             this->centroid.push_back(centroid.getVal(i));
         }
         this->addPoint(centroid);
+    }
+    vector<double> getVariance()
+    {
+        calculateVariance();
+        return variance;
+    }
+    void calculateVariance()
+    {
+        int i;
+        variance.resize(1);
+        for(i=0;i<variance.size();i++) variance[i]=0.0;
+        for(i=0;i<points.size();i++)
+        {
+            Point pt=points[i];
+            int d=centroid.size();
+            for(int j=0;j<d;j++)
+            {
+                variance[0]+=pow(pt.getVal(j)-centroid[j],2.0);
+            }
+            variance[0]=sqrt(variance[0])/(d * d);
+
+        }
     }
 
     void addPoint(Point p){
@@ -268,12 +290,205 @@ public:
         }
     }
 };
+typedef vector<double> Data;
+
+
+void Kmeans(double * data_vectors, double * centers,
+            double * variances, int m, int n, int K)
+{
+    int i=0;
+    int j=0;
+    int l=0;
+    int k=0;
+    double * new_centers = (double*)malloc(sizeof(double)*K*n);
+    int **cluster_members=new int*[K];
+    for(int i=0;i<K;i++)
+        cluster_members[i]=new int[m];
+    int *num_of_cluster_members=new int[K];
+    double distance=0;
+    double total_distance=0;
+    double min_distance=0;
+    int min_center=0;
+    int match=0;
+    int cur_match=0;
+    int new_cen=0;
+    int *random_centers=new int[K];
+    int found=1;
+    int iterations=0;
+
+
+    // Assign a random center to each example in the training set
+    for(i=0; i<K; i++)
+    {
+        do{
+            random_centers[i]=(int)((m-1) * drand48());
+        }while(random_centers[i]>m);
+
+    }
+    // Search the dataset and assign duplicate examples to different centers
+    for(i=0; i<K; i++)
+        for(j=0; j<K; j++)
+            if(i!=j)
+            if(random_centers[i] == random_centers[j]) {
+                for(l=0; l<m; l++) {
+                found=1;
+                for(k=0; k<K; k++) {
+                    if(l==random_centers[k])
+                        found=0;
+                }
+                if(found==1) {
+                    new_cen=l;
+                    break;
+                }
+                }
+                random_centers[j]=new_cen;
+            }
+    //for(i=0; i<K; i++)  printf("Random center is: %d %d\n", i, random_centers[i]);
+
+
+    // Create the initial random centers
+    for(i=0; i<K; i++) {
+        for(j=0; j<n; j++) {
+            //if(random_centers[i]==m)
+            //	printf("error \n");
+          centers[i*n + j] = data_vectors[random_centers[i]*n + j];
+          new_centers[i*n + j] = 0;
+          variances[i*n + j] = 0;
+        }
+        num_of_cluster_members[i]=0;
+        for(j=0; j<m; j++)
+            cluster_members[i][j]=0;
+    }
+
+    // Main K-Means loop starts here
+    iterations=0;
+    while(1) {
+
+    /* Loop over all points in the dataset */
+    for(i=0; i<m; i++) {
+
+       /* Estimate the closest center to point i */
+       for(j=0; j<K; j++) {
+        distance=0;
+        for(l=0; l<n; l++) {
+            distance += pow((data_vectors[i*n + l] - centers[j*n + l]),2.0);
+        }
+
+        if(j==0) {
+           min_distance = distance;
+           min_center = j;
+           continue;
+        }
+
+        if(distance < min_distance) {
+           min_distance = distance;
+           min_center = j;
+        }
+       }
+
+       for(l=0; l<n; l++)
+        new_centers[min_center*n + l] += data_vectors[i*n + l];
+       cluster_members[min_center][num_of_cluster_members[min_center]] = i;
+       num_of_cluster_members[min_center]++;
+
+
+    }
+
+       /* Estimate the new centers */
+       for(i=0; i<K; i++) {
+        for(l=0; l<n; l++) {
+            //GIANNIS
+            if(num_of_cluster_members[i])
+           new_centers[i*n + l] /= (double)num_of_cluster_members[i];
+            //GIANNIS
+           if(finite(new_centers[i*n + l]) == 0)
+            new_centers[i*n + l] = 0;
+        }
+       }
+
+    //for(i=0; i<K; i++) {
+    //	printf("Cluster members [%d]:  %d\n", i, num_of_cluster_members[i]);
+    //}
+
+
+    /* Here we print the total distance for each pass */
+    for(i=0; i<K; i++) {
+      for(j=0; j<num_of_cluster_members[i]; j++) {
+        for(l=0; l<n; l++) {
+        total_distance += pow((data_vectors[cluster_members[i][j]*n + l] - centers[i*n + l]),2.0);
+        }
+      }
+    }
+    //printf("Total distance: %f\n", total_distance);
+    total_distance=0;
+
+       /* Check if converges */
+       match=0;
+       for(i=0; i<K; i++) {
+        cur_match=0;
+        for(j=0; j<n; j++) {
+           if(new_centers[i*n + j] == centers[i*n + j])
+            cur_match++;
+        }
+        if(cur_match == n)
+            match++;
+       }
+
+       /* If the centers remain the same: terminate */
+       if(match == K)
+        break;
+
+
+       for(i=0; i<K; i++) {
+        for(j=0; j<n; j++) {
+            centers[i*n + j] = new_centers[i*n + j];
+            new_centers[i*n + j]=0;
+        }
+        num_of_cluster_members[i]=0;
+       }
+
+       iterations++;
+       if(iterations>2000) break;
+    }                        /* telos tou while(1) */
+
+
+    /* ----------------- YPOLOGISMOS VARIANCE -------------- */
+
+    for(i=0; i<K; i++) {
+        for(j=0; j<num_of_cluster_members[i]; j++) {
+            for(l=0; l<n; l++) {
+                variances[i*n + l] += pow((data_vectors[cluster_members[i][j]*n + l] - centers[i*n + l]),2.0);
+            }
+        }
+    }
+
+    for(i=0; i<K; i++) {
+        for(j=0; j<n; j++) {
+            //GIANNIS
+            if(num_of_cluster_members[i]>1)
+            variances[i*n + j] /= (double)num_of_cluster_members[i];
+            else
+            {
+                variances[i*n+j]=0.01;
+            }
+        }
+    }
+
+
+
+   free(new_centers);
+   delete[] num_of_cluster_members;
+   delete[] random_centers;
+   for(int i=0;i<K;i++) delete[] cluster_members[i];
+   delete[] cluster_members;
+}
+
+
 
 QString trainName="xy.data";
 QString testName="xy.data";
 int nodes=10;
 int dimension=0;
-typedef vector<double> Data;
 vector<Data> trainx;
 Data trainy;
 vector<Data> testx;
@@ -330,6 +545,9 @@ void setParameter(QString name,QVariant value)
     }
 }
 
+double *centers=NULL;
+double *variances=NULL;
+
 void loadTest()
 {
     QFile fp(testName);
@@ -368,33 +586,17 @@ void    init(QJsonObject data)
     if(testName!="xy.data") loadTest();
 
 #ifdef KMEANS
-    //Fetching number of clusters
-    int K = nodes;
-
-    //Open file for fetching points
-    string filename = trainName.toStdString();
-    ifstream infile(filename.c_str());
-    infile.is_open();
-
-    //Fetching points from file
-    int pointId = 1;
-    vector<Point> all_points;
-    string line;
-    int linecount=0;
-    while(getline(infile, line)){
-    linecount++;
-    if(linecount<=2) continue;
-        Point point(pointId, line);
-        all_points.push_back(point);
-        pointId++;
+    double *data_vectors=new double[ trainx.size() * trainx[0].size()];
+    centers=new double[nodes * trainx[0].size()];
+    variances=new double[nodes * trainx[0].size()];
+    int icount=0;
+    for(int i=0;i<trainx.size();i++)
+    {
+        for(int j=0;j<trainx[0].size();j++)
+            data_vectors[icount++]=trainx[i][j];
     }
-    infile.close();
-    //Running K-Means Clustering
-      int iters = 100;
-
-      KMeans kmeans(K, iters);
-      kmeans.run(all_points);
-       clusters=kmeans.getClusters();
+    Kmeans(data_vectors,centers,variances,trainx.size(),trainx[0].size(),nodes);
+    delete[] data_vectors;
 #endif
 }
 
@@ -409,17 +611,33 @@ void 	getmargins(vector<Interval> &x)
     {
             x[i]=Interval(initialLeft,initialRight);
     }
-    if(clusters.size()!=0)
+    if(variances)
     {
-        int icount=0;
-        for(int i=0;i<clusters.size();i++)
-        {
-            vector<double> cx=clusters[i].getCentroid();
-            for(int j=0;j<cx.size();j++)
+            int icount=0;
+            for(int i=0;i<nodes;i++)
             {
-                x[icount++]=Interval(-2.0*fabs(cx[j]),2.0*fabs(cx[j]));
+                for(int j=0;j<trainx[0].size();j++)
+                {
+
+                    double cx=centers[i * trainx[0].size()+j];
+                    x[icount++]=Interval(-5.0 * fabs(cx),
+                                          5.0 * fabs(cx));
+                }
             }
-        }
+
+            for(int i=0;i<nodes;i++)
+            {
+                double norm=0.0;
+                for(int j=0;j<trainx[0].size();j++)
+                {
+                    double vx=variances[i * trainx[0].size()+j];
+                    norm = norm  + vx * vx;
+
+                }
+                x[icount++]=Interval(-5.0*sqrt(norm),5.0 * sqrt(norm));
+            }
+            delete[] centers;
+            delete[] variances;
     }
 }
 
@@ -430,7 +648,7 @@ double neuronOutput( vector<double> &x, vector<double> &patt, unsigned pattDim, 
         out += (patt[i] - x[offset*pattDim + i]) * (patt[i] - x[offset*pattDim + i]);
     }
     double df=(-out / (x[nodes*pattDim+offset] * x[nodes*pattDim+offset]) );
-    if(fabs(df)>100) return 1e+8;
+    //if(fabs(df)>100) return 1e+8;
     //if(fabs(df)>100)return 1.0;// return 1000;
     return exp(df);
 }
@@ -476,7 +694,7 @@ adept::adouble aneuronOutput( vector<adept::adouble> &x, vector<double> &patt, u
         out += (patt[i] - x[offset*pattDim + i]) * (patt[i] - x[offset*pattDim + i]);
     }
     adept::adouble darg = out / (x[nodes*pattDim + offset] * x[nodes*pattDim + offset]);
-    if(fabs(darg)>100) return 1e+8;
+    //if(fabs(darg)>100) return 1e+8;
     return exp(-out / (x[nodes*pattDim + offset] * x[nodes*pattDim + offset]) );
 }
 
