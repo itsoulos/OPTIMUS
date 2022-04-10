010c427cd407c9f0349e5abd38837d8d0dc7116f
Ioannis G. Tsoulos <itsoulos@gmail.com>
2022-04-10 19:30:09 +0300

    Add Hill Climbing method

--- Optimus/optimizer.cpp
+++ Optimus/optimizer.cpp
@@ -4,7 +4,7 @@
 Optimizer::Optimizer(Problem *p)
 {
     myProblem=p;
-    addParameter("localsearch_method","bfgs","The desired local search method(bfgs,gradient,adam,lbfgs)");
+    addParameter("localsearch_method","bfgs","The desired local search method(bfgs,gradient,adam,lbfgs,grs,hill)");
     addParameter("bfgs_iterations","2001","The maximum number of iterations for BFGS");
     addParameter("gradient_rate","0.01","The learning rate for gradient descent");
     addParameter("gradient_iterations","10000","The maximum number of iterations for gradient descent");
@@ -78,6 +78,63 @@ double  Optimizer::localSearch(Data &x)
         x = n.getBestX();
         y = n.getBestY();
 
+    }
+    else
+    if(method=="hill")
+    {
+       Data currentPoint=x;    // the zero-magnitude vector is common
+       Data stepSize;
+       stepSize.resize(x.size());
+        for(int i=0;i<x.size();i++) stepSize[i]=1.0;
+       double acceleration=1.2;// // a value such as 1.2 is common
+       Data candidate;
+       candidate.resize(4);
+    candidate[0] = -acceleration;
+    candidate[1] = -1 / acceleration;
+    candidate[2] = 1 / acceleration;
+    candidate[3] = acceleration;
+    double bestScore = myProblem->funmin(currentPoint);
+    const int itermax=100;
+    int iter=1;
+    while(true)
+    {
+        bool termflag=false;
+        double beforeScore = bestScore;
+        for(int i=0;i<currentPoint.size();i++)
+        {
+
+            double beforePoint = currentPoint[i];
+            double bestStep = 0;
+            for(int j=0;j<4;j++)
+            {// try each of 4 candidate locations
+                double step = stepSize[i] * candidate[j];
+                currentPoint[i] = beforePoint + step;
+                double score = myProblem->funmin(currentPoint);
+                if( score < bestScore){
+
+                    bestScore = score;
+                    bestStep = step;
+                }
+            }
+            if(fabs(bestStep)<1e-5){
+                currentPoint[i] = beforePoint;
+                stepSize[i] = stepSize[i] / acceleration;
+            }
+            else
+            {
+                currentPoint[i] = beforePoint + bestStep;
+                stepSize[i] = bestStep; // acceleration
+            }
+          //  printf("Best score = %lf oldScore  = %lf iter =%d \n",bestScore,beforeScore);
+        if (fabs(bestScore-beforeScore) < 1e-5 || iter>itermax)
+        {termflag=true;
+            break;}
+            iter++;
+        }
+        if(termflag) break;
+    }
+    x=currentPoint;
+    return myProblem->funmin(x);
     }
 }
 
--- bin/rungenclass.sh
+++ bin/rungenclass.sh
@@ -10,7 +10,7 @@ then
 	METHODPARAMS="--minfinder_samples=25 --minfinder_sampling=repulsion"
 elif [ $METHOD = "Genetic" ]
 then
-	METHODPARAMS="--chromosome_count=500 --max_generations=500 --localsearch_method=grs --localsearch_rate=0.01 --localsearch_method=bfgs"
+	METHODPARAMS="--chromosome_count=500 --max_generations=500 --localsearch_method=grs --localsearch_rate=0.01 --localsearch_method=hill"
 elif [ $METHOD = "Pso" ]
 then
 	METHODPARAMS="--pso_localsearch_rate=0.05"
@@ -19,7 +19,7 @@ then
 	METHODPARAMS=="--multistart_samples=25"
 fi
 
-PROBLEM=DGenclass/libDGenClass.so
+PROBLEM=GenClass/libGenClass.so
 
 MLPARAMS="--trainName=file://$DATAPATH/$1.train --testName=file://$DATAPATH/$1.test "
 echo ./OptimusApp --filename=../PROBLEMS/$PROBLEM  --opt_method=$METHOD   $METHODPARAMS  $MLPARAMS --threads=12 
